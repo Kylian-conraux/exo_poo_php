15ea18a31ead89fcdc77efc181d39630a09444d2
kylian <kylianconraux@hotmail.fr>
2025-02-20 08:53:01 +0100

    ajout de Quatre_roues

---
+++ Quatre_roues.php
@@ -0,0 +1,33 @@
+<?php
+
+class Quatre_roues extends Vehicule{
+
+    private $nb_portes;
+
+    public function __construct($couleur, $poids, $nb_portes){
+        parent::__construct($couleur, $poids);
+        $this->nb_portes = $nb_portes;
+    }
+
+    public function get_nb_portes(){
+        return $this->nb_portes;
+    }
+
+    public function set_nb_portes($nb_portes){
+        $this->nb_portes = $nb_portes;
+    }
+
+    public function rouler(){
+        echo "Le quatre roues roule";
+    }
+
+    public function ajouterPersonne(){
+        echo "Une personne a été ajoutée";
+    }
+
+   public function repeindre($couleur){
+        $this->set_couleur($couleur);
+        echo "Le véhicule a été repeint en $couleur";
+    }
+
+}

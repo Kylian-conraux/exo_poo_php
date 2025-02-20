52cb87044bdd545260a05ba9efde6fc67e380529
kylian <kylianconraux@hotmail.fr>
2025-02-20 09:31:55 +0100

    ajout et affichage voiture verte

--- affichage.php
+++ affichage.php
@@ -6,7 +6,7 @@
     <title>Affichage</title>
 </head>
 <body>
-        <h1>Affichage d'un véhicule : </h1>
+        <h1>Affichage d'un véhicule Noire : </h1>
         <?php
         include_once 'allInclude.php';
         $voiture_noir = new Vehicule('noir', 1500);
@@ -18,6 +18,25 @@
         echo "<br>";
         echo "Le poids total est de " . $voiture_noir->get_poids() . " kg";
         echo "<br>"; 
+        ?>
+
+        <h1>Affichage d'une Voiture vert : </h1>
+        <?php
+        $voiture_vert = new Voiture('vert', 1400, 5, 0);
+        echo "La voiture est de couleur " . $voiture_vert->get_couleur() . " et pèse " . $voiture_vert->get_poids() . " kg, elle a " . $voiture_vert->get_nb_portes() . " portes et " . $voiture_vert->get_nombre_pneu_neige() . " pneus neige";
+        echo "<br>";
+        $voiture_vert->ajouterPersonne(65);
+        echo "<br>";
+        $voiture_vert->ajouterPersonne(65);
+        echo "<br>";
+        echo "Le poids total est de " . $voiture_vert->get_poids() . " kg et sa couleur est " . $voiture_vert->get_couleur();
+        echo "<br>";
+        $voiture_noir->repeindre('rouge');
+        echo "<br>";
+        $voiture_vert->ajouter_pneu_neige(2);
+        echo "<br>";
+        echo "Il y a " . $voiture_vert->get_nombre_pneu_neige() . " pneus neige et elle est de couleur " . $voiture_vert->get_couleur();
+        echo "<br>";
         ?>
 </body>
 </html>
